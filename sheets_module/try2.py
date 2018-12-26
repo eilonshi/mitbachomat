a4c88cdd90bb63f79a1b93d6f90659d97de03a99
t8455242 <yanir.ravia@mail.huji.ac.il>
2018-12-27 00:11:42 +0200

    add product function

--- sheets_module/try2.py
+++ sheets_module/try2.py
@@ -8,6 +8,14 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name('client_secret.js
 
 gc = gspread.authorize(credentials)
 
-wks = gc.open('try').sheet1
+wks = gc.open('מטבחומט - מצב המטבחון')
 
-print(wks.get_all_records())
+def add_product_push(product_name, date_and_time,push_button_number,value):
+    sheet = wks.worksheet(product_name)
+    data = sheet.get_all_values()
+    cell_to_add = len(data)+1
+    sheet.update_acell('A'+str(cell_to_add),date_and_time)
+    sheet.update_acell('B' + str(cell_to_add),'button'+str(push_button_number))
+    sheet.update_acell('C' + str(cell_to_add),int(sheet.acell('C'+str(cell_to_add-1)).value)+value)
+
+add_product_push('גבנצ','15/9/19',5,6000000000)
